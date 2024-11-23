26201a6adffa9a55cd56ea39ec6fe00c4a4764b3
Vitor Ribeiro <167051938+Vitor-Ribe@users.noreply.github.com>
2024-11-23 19:41:55 -0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,12 +1,12 @@
 import socket
 
-host = "192.168.---.---"  # IP do servidor (ESP)
+host = "192.168.100.189"  # IP do servidor (ESP)
 port = 80  # Porta de comunicação (deve ser a mesma que o servidor usa)
 
 def enviar_comando(comando):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        s.sendall((comando + '\n').encode())  # Envia o comando para o servidor
+        s.sendall(comando.encode())  # Envia o comando para o servidor
         resposta = s.recv(1024).decode()  # Recebe a resposta do servidor
         print(resposta)  # Exibe a resposta no cliente
 
